avar/sha1collisiondetection

commit 33a694a9ee1b79c24be45f9eab5ac0e1aeeaf271

Fix issues with a big endian platform

lib/sha1.c
@@ -28,7 +28,7 @@
 #if (!defined SHA1DC_FORCE_LITTLEENDIAN) && \
     ((defined(__BYTE_ORDER) && (__BYTE_ORDER == __BIG_ENDIAN)) || \
     (defined(__BYTE_ORDER__) && (__BYTE_ORDER__ == __BIG_ENDIAN__)) || \
-    defined(__BIG_ENDIAN__) || defined(__ARMEB__) || defined(__THUMBEB__) ||  defined(__AARCH64EB__) || \
+    defined(_BIG_ENDIAN) || defined(__BIG_ENDIAN__) || defined(__ARMEB__) || defined(__THUMBEB__) ||  defined(__AARCH64EB__) || \
     defined(_MIPSEB) || defined(__MIPSEB) || defined(__MIPSEB__) || defined(SHA1DC_FORCE_BIGENDIAN))
 
 #define SHA1DC_BIGENDIAN
@@ -1744,7 +1744,8 @@ void SHA1DCUpdate(SHA1_CTX* ctx, const char* buf, size_t len)
 	while (len >= 64)
 	{
 		ctx->total += 64;
-		sha1_process(ctx, (uint32_t*)(buf));
+		memcpy(ctx->buffer, buf, 64);
+		sha1_process(ctx, (uint32_t*)(ctx->buffer));
 		buf += 64;
 		len -= 64;
 	}

commit fd166aacc1fd4c6af2fa745e10f2cf877d0992b8

Big Endian detection: try to un-break on *BSD and newlib
This is a rebase/squash of the following commits for ease of
readability (there were merges etc. in-between):

    commit 0efca96
    Author: SODA Noriyuki <soda@NetBSD.org>
    Date:   Thu May 25 14:49:42 2017 +0900

        include <sys/types.h> explicitly if __unix__, to make sure macros visible

        the _BIG_ENDIAN macro on NetBSD 9 and Solaris 9 are already visible here.
        but it's because <sys/types.h> is included indirectly via <stdlib.h>
        or <memory.h>.  that's fragile against changes in those headers.
        pointed out by obache-san

    commit c09e03e
    Author: SODA Noriyuki <soda@NetBSD.org>
    Date:   Thu May 25 14:49:05 2017 +0900

        use "_BIG_ENDIAN" symbol on Solaris

        revive the "_BIG_ENDIAN" symbol support on Solaris,
        which was removed in commit 947dd3589ea4
        pointed out by obache-san

    commit 947dd35
    Author: SODA Noriyuki <soda@NetBSD.org>
    Date:   Thu May 25 12:30:47 2017 +0900

        commit 33a694a9ee1b broke all little endian platforms on *BSD,
        because _BIG_ENDIAN is always defined even on little endian platforms.

        problem found by nonaka at NetBSD.org

The _BIG_ENDIAN on Solaris change didn't make it.

lib/sha1.c
@@ -10,6 +10,9 @@
 #include <memory.h>
 #include <stdio.h>
 #include <stdlib.h>
+#ifdef __unix__
+#include <sys/types.h> /* make sure macros like _BIG_ENDIAN visible */
+#endif
 #endif
 
 #ifdef SHA1DC_CUSTOM_INCLUDE_SHA1_C
@@ -69,6 +72,13 @@
 
 #else /* Not under GCC-alike or glibc */
 
+/* *BSD and newlib (embeded linux, cygwin, etc) */
+#if defined(_BYTE_ORDER) && defined(_BIG_ENDIAN) && _BYTE_ORDER == _BIG_ENDIAN
+
+#define SHA1DC_BIGENDIAN
+
+#else /* Not under GCC-alike or glibc or *BSD or newlib or <processor whitelist> */
+
 #if (defined(__ARMEB__) || defined(__THUMBEB__) || defined(__AARCH64EB__) || \
      defined(__MIPSEB__) || defined(__MIPSEB) || defined(_MIPSEB) || \
      defined(__sparc))
@@ -79,21 +89,23 @@
  */
 #define SHA1DC_BIGENDIAN
 
-#else /* Not under GCC-alike or glibc or <processor whitelist> */
+#else /* Not under GCC-alike or glibc or *BSD or newlib or <processor whitelist> */
 
 #if defined(SHA1DC_ON_INTEL_LIKE_PROCESSOR)
+
 /*
  * As a last resort before we do anything else we're not 100% sure
  * about below, we blacklist specific processors here. We could add
  * more, see e.g. https://wiki.debian.org/ArchitectureSpecificsMemo
  */
-#else /* Not under GCC-alike or glibc or <processor whitelist>  or <processor blacklist> */
+#else /* Not under GCC-alike or glibc or *BSD or newlib or <processor whitelist>  or <processor blacklist> */
 
 /* We do nothing more here for now */
 /*#error "Uncomment this to see if you fall through all the detection"*/
 
 #endif /* !SHA1DC_ON_INTEL_LIKE_PROCESSOR */
 #endif /* Big Endian under whitelist of processors */
+#endif /* Big Endian under *BSD and newlib */
 #endif /* Big Endian under glibc */
 #endif /* Big Endian under GCC-alike */
 